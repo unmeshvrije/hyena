3129e1d507aa711b93d1221f44de812ba74e1b75
unmeshvrije <unmesh.joshi126@gmail.com>
2023-07-25 14:38:57 +0200

    Remove HTML 5 boilerplate

--- js/main.js
+++
@@ -1,7 +0,0 @@
-const queryClick = (event) => {
-    console.log("button is clicked");
-    queryStr = document.createTextNode("? bornIn Amsterdam");
-    document.getElementById("content").appendChild(queryStr);
-};
-
-document.getElementById("query").addEventListener('click', queryClick)
