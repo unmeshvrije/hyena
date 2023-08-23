e9430e8fb5c5539b0bf50e20b4e5e53a87a6e420
unmeshvrije <unmesh.joshi126@gmail.com>
2023-08-23 13:09:40 +0200

    Show queries in columns using Angular component

--- src/app/query/query.component.ts
+++ src/app/query/query.component.ts
@@ -1,20 +1,40 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import queries from "../../../dbpedia50_head_queries.json"
+
+type QueryObject  = {
+  [key: string]: string;
+};
 
 @Component({
   selector: 'app-query',
   standalone: true,
   imports: [CommonModule],
   template: `
-    <section>
-      <form>
-        <input type="text" placeholder="Filter by query">
-        <button class="primary" type="button"> Generate Random Query </button> 
-      </form>
-    </section>
+  <table>
+      <tr>
+        <th *ngFor="let col of colDefs">{{col.label}}</th>
+        <th> Short Answer </th>
+      </tr>
+
+      <tr *ngFor="let row of data">
+        <td *ngFor="let col of colDefs">{{row[col.field]}}</td>
+        <td> Short answer </td>
+      </tr>
+    </table>
   `,
   styleUrls: ['./query.component.css']
 })
-export class QueryComponent {
 
+export class QueryComponent {
+  // column definitions
+  colDefs = [
+  { label: "Query", field: "query" },
+  { label: "Answer", field: "answer" },
+  //{ label: "Short Answer", field: "short" },
+  //{ label: "Long Answer", field: "long" },
+  ];
+
+  // datasource
+  data: Array<QueryObject> = queries;
 }
